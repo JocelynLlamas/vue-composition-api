93824b02c0e1e435ed0d4fa827997390301554ba
Jocelyn Llamas <mjlldlt@hotmail.com>
2023-11-16 11:41:50 -0600

    Computed Propierties

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
 <script>
-import {reactive, ref, toRef, toRefs} from 'vue';
+import { reactive, ref, toRef, toRefs, computed } from 'vue';
 
-export default{
-  setup(){
+export default {
+  setup() {
 
     const message = ref('Hello')
     // If you are working with simple values, use ref function
-    const quantity = ref(1)
+    // const quantity = ref(1)
     // For more complex data structures or objects with multiple
     // propierties, use reactive function
     const item = reactive({
       name: "Product 1",
-      price: 10
+      price: 10,
+      quantity: 1,
     })
 
-    const increment = () => quantity.value++
-    const decrement = () =>quantity.value--
+    const increment = () => item.quantity++
+    const decrement = () => item.quantity--
     const swapProduct = () => {
       item.name = "Product A"
       item.price = 30
     }
-    
+
+
+    const total = computed(()=> item.price * item.quantity)
+
     // const nameRef = toRef(item, 'name');
     // console.log('nameRef:', nameRef.value)
     // item.name = "New Product"
     // console.log('nameRef:', nameRef.value)
 
-    const {name, price} = toRefs(item)
+    const { name, price, quantity } = toRefs(item)
 
     // console.log('name:', itemRefs.name.value)
     // console.log('price:', itemRefs.price.value)
-    
+
     // item.name = "Hot Product"
     // item.price = 50
 
     // console.log('name:', itemRefs.name.value)
     // console.log('price:', itemRefs.price.value)
 
-    return{
+    return {
       message,
       quantity,
       name,
       price,
+      total,
       increment,
       decrement,
       swapProduct
@@ -53,10 +58,12 @@ export default{
 <template>
   <h1>{{ name }} : {{ price }}</h1>
   <button @click="swapProduct">Swap Product</button>
+  <button @click="price++">Increment Price</button>
   <h2>{{ quantity }}</h2>
   <button @click="increment">+</button>
   <button @click="decrement">-</button>
+
+  <h3>Total: {{ total }}</h3>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
