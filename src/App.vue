513957dd924cb3bec0b80a55531b3f7a64ec7760
Jocelyn Llamas <mjlldlt@hotmail.com>
2023-11-15 22:55:04 -0600

    Composition API Setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+  setup(){
+    const message = 'Hello'
+
+
+    return{
+      message
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+</style>
